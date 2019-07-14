e54b49cbfefd60051359fa3d23a50d0b73681fe6
Ricardo Colzani <6742811+rcolzani@users.noreply.github.com>
2019-07-14 19:16:15 -0300

    feature: create appointment with provider

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -15,7 +16,8 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
